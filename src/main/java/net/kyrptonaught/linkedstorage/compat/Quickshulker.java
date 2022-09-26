0b52ddfecac9f29df332d331dc9e5143405aba7c
kyrptonaught <anthonybuttillo10@gmail.com>
2022-09-26 16:52:41 -0400

    ADd quickshulker support

---
+++ src/main/java/net/kyrptonaught/linkedstorage/compat/Quickshulker.java
@@ -0,0 +1,24 @@
+package net.kyrptonaught.linkedstorage.compat;
+
+import net.kyrptonaught.linkedstorage.LinkedStorageMod;
+import net.kyrptonaught.linkedstorage.inventory.LinkedContainer;
+import net.kyrptonaught.linkedstorage.item.StorageItem;
+import net.kyrptonaught.linkedstorage.util.LinkedInventoryHelper;
+import net.kyrptonaught.quickshulker.QuickShulkerMod;
+import net.kyrptonaught.quickshulker.api.QuickOpenableRegistry;
+import net.kyrptonaught.quickshulker.api.RegisterQuickShulker;
+
+public class Quickshulker implements RegisterQuickShulker {
+    @Override
+    public void registerProviders() {
+        new QuickOpenableRegistry.Builder()
+                .setItem(StorageItem.class)
+                .supportsBundleing(true)
+                .getBundleInv((player, stack) -> LinkedStorageMod.getInventory(LinkedInventoryHelper.getItemChannel(stack)))
+                .setOpenAction((player, stack) -> {
+                    player.openHandledScreen(LinkedContainer.createScreenHandlerFactory(LinkedInventoryHelper.getItemChannel(stack)));
+                })
+                .canOpenInHand(false)
+                .register();
+    }
+}
